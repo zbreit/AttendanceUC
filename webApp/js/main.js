29d3ad0b2699f0eb84c9a808fddd7eb5fd67534b
Zach Breit <zbreit18@gmail.com>
2017-05-26 22:25:58 -0400

    Added regex validation for barcodes. Fixed the animation timing function.

--- webApp/js/main.js
+++ webApp/js/main.js
@@ -1,7 +1,42 @@
 var barcodeInput = document.getElementById('barcode-input');
+var prevVal = barcodeInput.value;
+var currentVal = barcodeInput.value;
+
 
 function focusBarcodeInput() {
     barcodeInput.focus();
 }
 
 document.addEventListener('DOMContentLoaded', focusBarcodeInput);
+
+function sendBarcodeIfValid() {
+    currentVal = barcodeInput.value;
+    if (hasInputChanged()) {
+        console.log('Input Value Change');
+        if (isValidBarcode(currentVal)) {
+            console.log('Valid Barcode')
+            sendBarcode(currentVal);
+        }
+    }
+}
+
+function hasInputChanged() {
+    if (currentVal !== prevVal) {
+        prevVal = currentVal;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isValidBarcode(barcodeInput) {
+    var barcodeRegEx = /\b\d{8}\b/
+    return barcodeRegEx.test(barcodeInput);
+}
+
+function sendBarcode(barcodeVal) {
+    //DUMMY FUNCTION
+    return;
+}
+
+var checker = setInterval('sendBarcodeIfValid()', 500);

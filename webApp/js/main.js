f2289d53d46f3ab7e3646cecf45f8b7915dc7db9
Zach Breit <zbreit18@gmail.com>
2017-06-10 23:45:08 -0400

    Temporary commit of web app changes.

--- webApp/js/main.js
+++ webApp/js/main.js
@@ -1,81 +1,172 @@
-var barcodeInput = document.getElementById('barcode-input');
-var prevVal = barcodeInput.value;
-var currentVal = barcodeInput.value;
-var eventList = [];
-var eventURL = 'https://api.airtable.com/v0/appAmvoUMZeuE3Avq/Events?api_key=';
-var airtableKey;
+//(function() { //Create a module to make all variables anonymous
+    var barcodeInput = document.getElementById('barcode-input');
+    var eventDropdown = document.getElementById('event-dropdown');
+    var prevVal = barcodeInput.value;
+    var currentVal = prevVal;
+    var baseURL = "https://api.airtable.com/v0/appAmvoUMZeuE3Avq/"
+    var apiKeyParam = "?api_key="
+    var eventURL = baseURL + 'Events' + apiKeyParam;
+    var attendanceURL = baseURL + "List%20of%20Atendees" + apiKeyParam;
+    var attendEventURL = baseURL + "List%20of%20Atendees/"
+    var airtableKey;
+    var eventDict;
+    var eventList = [];
+    var barcodeList = [];
+    var participantList = [];
+    var participantEvents = [];
+    var gatheredApiKey = new Event('apiKeyRetrieved');
+    var gatheredEventList = new Event('eventListRetrieved');
 
-function getApiKey() {
-    var xhr = new XMLHttpRequest();
-    var file_data = {};
-    var api_key = '';
+    document.addEventListener('apiKeyRetrieved', function() {
+        getEventList();
+        getBarcodeList();
+    }, false);
 
-    xhr.open('GET', 'http://localhost:8000/js/config.json', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            file_data = JSON.parse(xhr.responseText);
-            airtableKey = file_data['api_key'];
+    document.addEventListener('eventListRetrieved', updateEventList, false);
+
+    function getBarcodeList() {
+        barcodeReq = new XMLHttpRequest();
+        barcodeEndpoint = attendanceURL + airtableKey
+        barcodeReq.open('GET', barcodeEndpoint, true);
+        barcodeReq.onreadystatechange = function() {
+            if (barcodeReq.readyState === 4 && barcodeReq.status === 200) {
+                listOfAttendees = JSON.parse(barcodeReq.responseText);
+                listOfAttendees['records'].forEach(function(participant, index, array) {
+                    participantId = participant['id'];
+                    barcodeNum = participant['fields']['Barcode']['text'];
+                    listOfEvents = participant['fields']['Events'];
+                    if(listOfEvents === undefined) {
+                        listOfEvents = [];
+                    }
+                    //Ensures that the name and barcode have the same index in their respective arrays
+                    barcodeList.push(barcodeNum);
+                    participantList.push(participantId);
+                    participantEvents.push(listOfEvents);
+                });
+            }
         }
-    };
-    xhr.send();
-}
-
-function focusBarcodeInput() {
-    barcodeInput.focus();
-}
-
-function hasInputChanged() {
-    if (currentVal !== prevVal) {
-        prevVal = currentVal;
-        return true;
-    } else {
-        return false;
+        barcodeReq.send();
     }
-}
 
-function isValidBarcode(barcodeInput) {
-    var barcodeRegEx = /\b\d{8}\b/ //Exactly 8 integers
-    return barcodeRegEx.test(barcodeInput);
-}
+    function getApiKey() {
+        var xhr = new XMLHttpRequest();
+        var file_data = {};
+        var api_key = '';
 
-function sendBarcodeIfValid() {
-    currentVal = barcodeInput.value;
-    if (hasInputChanged()) {
-        console.log('Input Value Change');
-        if (isValidBarcode(currentVal)) {
-            console.log('Valid Barcode')
-            sendBarcode(currentVal);
+        xhr.open('GET', 'http://212.237.22.247/config.json', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                file_data = JSON.parse(xhr.responseText);
+                airtableKey = file_data['api_key'];
+                document.dispatchEvent(gatheredApiKey);
+            }
+        };
+        xhr.send();
+        //airtableKey = 'keyMBUvruNJcIPtjx';
+        //document.dispatchEvent(gatheredApiKey);
+    }
+
+    function focusBarcodeInput() {
+        barcodeInput.focus();
+    }
+
+    function hasInputChanged() {
+        if (currentVal !== prevVal) {
+            prevVal = currentVal;
+            return true;
+        } else {
+            return false;
         }
     }
-}
 
-function sendBarcode(barcodeVal) {
-    //DUMMY FUNCTION
-    return;
-}
+    function isValidBarcode(barcodeInput) {
+        var barcodeRegEx = /\b\d{8}\b/ //Exactly 8 integers
+        var passesRegex = barcodeRegEx.test(barcodeInput);
+        var barcodeInList = barcodeList.includes(barcodeInput); //If the barcode can be found in the list of barcodes
+        return (passesRegex && barcodeInList);
+    }
 
-function getEventList() {
-    var airtableGetRequest = new XMLHttpRequest();
-    var airtableEndPoint = eventURL + airtableKey
-    airtableGetRequest.open('GET', airtableEndPoint, true); //TODO: fix API key security issue
-    airtableGetRequest.onreadystatechange = function() {
-        if (airtableGetRequest.readyState === 4 && airtableGetRequest.status === 200) {
-            console.log("Connected to airtable");
-            console.log(airtableGetRequest.responseText);
+    function sendBarcodeIfValid() {
+        currentVal = barcodeInput.value;
+        if (hasInputChanged()) {
+            console.log('Input Value Change');
+            if (isValidBarcode(currentVal)) {
+                console.log('Valid Barcode')
+                attendEvent(currentVal);
+            }
         }
     }
-    airtableGetRequest.send();
 
-}
+    function attendEvent(barcodeVal) {
+        var eventID = getCurrentEventID();
+        var particpantIndex = getParticipantIndex(barcodeVal);
+        var participantId = getParticipantId(particpantIndex);
+        var attendReq = new XMLHttpRequest();
+        participantEvents[particpantIndex].push(eventID);
+        var paramObj = {
+            "fields" : {
+                "Events": participantEvents[particpantIndex]
+            }
+        };
+        var eventParam = '&json=' +  JSON.stringify(paramObj);
+        var attendReqEndpoint = attendEventURL + participantId + apiKeyParam + airtableKey + eventParam;
+        attendReq.open('PATCH', attendReqEndpoint, true);
+        attendReq.onreadystatechange = function() {
+            if (attendReq.readyState === 4 && attendReq.status === 200) {
+                console.log("Attended Event!");
+
+            }
+        };
+        attendReq.send();
+    }
+
+    function getCurrentEventID() {
+        return eventDropdown.options[eventDropdown.selectedIndex].value;
+    }
+
+    function getParticipantIndex(barcodeNum) {
+        return barcodeList.indexOf(barcodeNum);
+    }
+
+    function getParticipantId(index) {
+
+        return participantList[index];
+    }
+
+    function getEventList() {
+        var airtableGetRequest = new XMLHttpRequest();
+        var airtableEndPoint = eventURL + airtableKey
+        airtableGetRequest.open('GET', airtableEndPoint, true);
+        airtableGetRequest.onreadystatechange = function() {
+            if (airtableGetRequest.readyState === 4 && airtableGetRequest.status === 200) {
+                console.log("Retrieved Event List");
+                eventDict = JSON.parse(airtableGetRequest.responseText);
+                document.dispatchEvent(gatheredEventList);
+            }
+        }
+        airtableGetRequest.send();
+    }
+
+    function updateEventList() {
+        var eventDropdown = document.getElementById('event-dropdown');
+        eventDict["records"].forEach(function(currentVal, index, array) {
+            var event = document.createElement("option");
+            var eventName = currentVal['fields']['Name'];
+            var eventId = currentVal['id'];
+            var eventText = document.createTextNode(eventName);
+            var valueAttribute = document.createAttribute('value');
+            valueAttribute.value = eventId;
+            event.setAttributeNode(valueAttribute);
+            event.appendChild(eventText);
+            eventDropdown.appendChild(event);
+        });
+    }
 
 
+    document.addEventListener('DOMContentLoaded', function() {
+        focusBarcodeInput();
+        getApiKey();
+    });
 
-document.addEventListener('DOMContentLoaded', function() {
-    focusBarcodeInput();
-    getApiKey();
-    setTimeout(function() {
-        console.log("waiting");
-    }, 1000);
-    eventList = getEventList();
-});
-var checker = setInterval('sendBarcodeIfValid()', 500);
+    var checker = setInterval(sendBarcodeIfValid, 500);
+// }());
